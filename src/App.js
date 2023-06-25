9d10ec93ca28e2e980276d85d5e7f85d267fc355
jetstream1128 <bogdanalexeev2007@live.com>
2023-06-25 12:53:24 -0600

    Refactoring

--- src/App.js
+++ src/App.js
@@ -1,5 +1,51 @@
-function App() {
-	return <div>Hello React</div>;
-}
+import { useState } from "react";
+import Logo from "./Logo";
+import Form from "./Form";
+import PackingList from "./PackingList";
+import Stats from "./Stats";
 
-export default App;
+const initialItems = [
+	{ id: 1, description: "Passports", quantity: 2, packed: false },
+	{ id: 2, description: "Socks", quantity: 12, packed: true },
+	{ id: 3, description: "Charger", quantity: 1, packed: false },
+];
+
+export default function App() {
+	const [items, setItems] = useState(initialItems);
+
+	function handleAddItems(item) {
+		setItems((items) => [...items, item]);
+	}
+
+	function handleDeleteItem(id) {
+		setItems((items) => items.filter((item) => item.id !== id));
+	}
+
+	function handleToggleItem(id) {
+		setItems((items) =>
+			items.map((item) =>
+				item.id === id ? { ...item, packed: !item.packed } : item
+			)
+		);
+	}
+	function handleDeleteAllItems() {
+		const confirmed = window.confirm(
+			"Are you sure you want to delete all items?"
+		);
+		if (confirmed) setItems([]);
+	}
+
+	return (
+		<div className="app">
+			<Logo />
+			<Form onAddItems={handleAddItems} />
+			<PackingList
+				items={items}
+				onDeleteItem={handleDeleteItem}
+				onToggleItems={handleToggleItem}
+				onDeleteAllItems={handleDeleteAllItems}
+			/>
+			<Stats items={items} />
+		</div>
+	);
+}
